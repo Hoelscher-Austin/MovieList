c49b721d18e67af1c8f08421b2957b27ac51ec9a
Austin Hoelscher <alhoelscher@mail.fhsu.edu>
2023-06-11 18:34:32 -0500

    Made index, add & edit view for Movie Controller. Added bootstrap icons cdn script to header in _layout. Messed with bootstrap and visuals.

--- MovieList/Areas/Admin/Controllers/MovieController.cs
+++ MovieList/Areas/Admin/Controllers/MovieController.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieList.DataAccess.Data;
+using MovieList.Models.Models;
 
 namespace MovieList.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class MovieController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+        //GET Actions
         public IActionResult Index()
         {
+            List<Movie> moviesList = _dbContext.Movies.ToList();
+            return View(moviesList);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        
+        public IActionResult Edit(int? id)
+        {
+
+            if(id == null || id == 0) {
+                return NotFound();
+            }
+            Movie? movie = _dbContext.Movies.FirstOrDefault(x => x.Id == id);
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+
+
+        //POST Actions
+
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Movies.Update(movie);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
